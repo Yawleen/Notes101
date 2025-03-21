f2440ae9d9df48c5a762a4d9dccba1e2bdcaf2d1
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-03-21 01:56:05 -0400

    Modification du chemin de l'URL à fetch dans le fichier notes.js

--- src/features/notes.js
+++ src/features/notes.js
@@ -33,7 +33,7 @@ export const notes = createSlice({
 
 export function getNotesList(action) {
   return function (dispatch, getState) {
-    fetch("/data/notes.json")
+    fetch("/site-portfolio/projets/projets-react/Notes101/data/notes.json")
       .then((data) => data.json())
       .then((data) => dispatch(getNotes(data.notes)));
   };
